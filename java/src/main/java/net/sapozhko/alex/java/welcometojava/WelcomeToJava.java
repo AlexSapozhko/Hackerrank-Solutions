e7d08f0df33741857650e9fa76a9a2daece815b7
Alex Sapozhko <alex.sapozhko@gmail.com>
2017-04-22 21:27:39 +0300

    Initial commit

---
+++ java/src/main/java/net/sapozhko/alex/java/welcometojava/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package net.sapozhko.alex.java.welcometojava;
+
+/**
+ * @author Alexander on 22.04.2017.
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
