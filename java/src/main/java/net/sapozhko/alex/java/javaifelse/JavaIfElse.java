23d8bba006d49f5eeb2f67f17f27e8efe3c2fb5a
Alex Sapozhko <alex.sapozhko@gmail.com>
2017-04-22 22:03:42 +0300

    added Java If-Else-Solution

---
+++ java/src/main/java/net/sapozhko/alex/java/javaifelse/JavaIfElse.java
@@ -0,0 +1,25 @@
+package net.sapozhko.alex.java.javaifelse;
+
+import java.util.Scanner;
+
+/**
+ * @author Alexander on 22.04.2017.
+ */
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ans = "";
+        if (n % 2 == 1) {
+            ans = "Weird";
+        } else if (n >= 2 && n <= 5) {
+            ans = "Not Weird";
+        } else if (n >= 6 && n <= 20) {
+            ans = "Weird";
+        } else {
+            ans = "Not Weird";
+        }
+
+        System.out.println(ans);
+    }
+}
